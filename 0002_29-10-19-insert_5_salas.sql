0f04641a2e9d6feb70d01b587c1e8dbffae80d82
Lisa <lisaevemarin17@gmail.com>
2019-11-11 19:51:43 -0300

    insert 5 salas

---
+++ 0002_29-10-19-insert_5_salas.sql
@@ -0,0 +1,5 @@
+INSERT INTO cine.salas (codigo, nombre, pelicula) VALUES (001, 'sala 1',1111 );
+INSERT INTO cine.salas (codigo, nombre, pelicula) VALUES (002, 'sala 2',2222);
+INSERT INTO cine.salas (codigo, nombre, pelicula) VALUES (003, 'sala 3',3333);
+INSERT INTO cine.salas (codigo, nombre, pelicula) VALUES (004, 'sala 4',4444);
+INSERT INTO cine.salas (codigo, nombre, pelicula) VALUES (005, 'sala 5',5555);
