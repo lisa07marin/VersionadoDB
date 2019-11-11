7d907d10016890a9fe43ef2daaa6c818f35f276e
Lisa <lisaevemarin17@gmail.com>
2019-11-11 20:01:08 -0300

    se actualiza el campo anio de la tabla pelicula

---
+++ 0004_29-10-19-update_pelicula.anio.sql
@@ -0,0 +1,5 @@
+UPDATE cine.peliculas SET año = 1984 WHERE p_codigo = 1111;
+UPDATE cine.peliculas SET año = 1999 WHERE p_codigo = 6666 ;
+UPDATE cine.peliculas SET año = 2001 WHERE p_codigo = 7777;
+UPDATE cine.peliculas SET año = 2008 WHERE p_codigo = 8888;
+UPDATE cine.peliculas SET año = 2010 WHERE p_codigo = 2333;
