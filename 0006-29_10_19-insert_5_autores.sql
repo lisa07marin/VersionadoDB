b3e695ea5939907a1287ef46f28ce3b4eeecb163
Lisa <lisaevemarin17@gmail.com>
2019-11-13 18:28:11 -0300

    se agregan 5 autores

---
+++ 0006-29_10_19-insert_5_autores.sql
@@ -0,0 +1,5 @@
+INSERT INTO cine.autores VALUES(1,'Stan Lee');
+INSERT INTO cine.autores VALUES(2,'Jack Kirby');
+INSERT INTO cine.autores VALUES(3,'Irene Mecchi');
+INSERT INTO cine.autores VALUES(4,'Jonathan Roberts ');
+INSERT INTO cine.autores VALUES(5,'Linda Woolverton');
